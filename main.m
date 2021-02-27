1653e13281c2745d78b9aee25151b339dfe69452
hauLLL2050 <65396258+hauLLL2050@users.noreply.github.com>
2021-02-27 14:59:56 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  lydiatest
+//
+//  Created by lydia on 2021/2/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+
+// this is test
+
+
+aaaadfsadfasdf
+
+
+        11111
+        222212
+        333
+        444
+    }
+    return 0;
+}
